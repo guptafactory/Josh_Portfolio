9779fac202a34714c445d7588f495b28d1d166e4
Himanshu Gupta <guptafactory@gmail.com>
2024-08-28 14:20:16 +0530

    Add swiper

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
+const menu = document.querySelector(".toggleMenu");
+const navigation = document.querySelector(".navigation");
+const navbar = document.querySelector("#navbar");
+const sticky = navbar.offsetTop;
+
 // Navbar starts
 function toggleMenu() {
-  const toggleMenu = document.querySelector(".toggleMenu");
-  const navigation = document.querySelector(".navigation");
-
-  toggleMenu.classList.toggle("active");
+  menu.classList.toggle("active");
   navigation.classList.toggle("active");
 }
-// When the user scrolls the page, execute myFunction
+// When the user scrolls the page, execute toggleSticky
 window.onscroll = function () {
-  myFunction();
+  toggleSticky();
 };
 
-// Get the navbar
-let navbar = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-let sticky = navbar.offsetTop;
-
 // Adding the sticky class to the navbar when reaching its scroll position. Remove "sticky" when leaving the scroll position
-function myFunction() {
+function toggleSticky() {
   if (window.scrollY > sticky) {
     navbar.classList.add("sticky");
   } else {
     navbar.classList.remove("sticky");
   }
 }
+// Navbar ends
+
+// Swiper starts
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    900: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+  // autoplay: {
+  //   delay: 1000,
+  // },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+// Swiper ends
